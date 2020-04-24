b812d7a1262cbe4a0a578f5eed261683f26f0692
talha915 <talhazafar915426@gmail.com>
2020-04-24 01:21:27 -0700

    All FInal

--- components/Home/Banner.jsx
+++ components/Home/Banner.jsx
@@ -11,13 +11,13 @@ class Banner extends Component {
                     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet" />
                     <link href="/static/style.css" rel="stylesheet" />
                 </Head>
-                <div className="visual" style={{backgroundImage: "url(images/img33.jpg)"}}>
+                <div className="visual" style={{ backgroundImage: "url(images/img33.jpg)" }}>
                     <div className="container">
                         <div className="textblock">
                             <h1>Software Designed To Make Growing Your Aesthetic Practice Easy</h1>
                             <p>All-In-One EMR & Practice Management Software Solution</p>
-                            <a href="#" className="btn">Schedule Demo</a>
-                            <a href="#" className="btn full">Start Free Trial</a>
+                            <a href="https://calendly.com/isabelle-mcdowell/aesthetic-record-demo?month=2020-04" target="_blank" className="btn">Schedule Demo</a>
+                            <a href="https://app.aestheticrecord.com/signup-basic?_ga=2.39061923.160160872.1587343952-1803440311.1579636487" target="_blank" className="btn full">Start Free Trial</a>
                             <em className="note">No Credit Card Required, 14 Day Free Trial</em>
                         </div>
                     </div>
